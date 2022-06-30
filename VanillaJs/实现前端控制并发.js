26065974d00c8a49281043eb90cf766e22b173e6
luantao <82134878@qq.com>
2022-06-30 16:45:31 +0800

    feat

--- VanillaJs/实现前端控制并发.js
+++ VanillaJs/实现前端控制并发.js
@@ -39,4 +39,23 @@ const loadByLimit = (urlIds, loadImg, limit) => {
         .then(() => Promise.all(promiseArray));
 };
 
-loadByLimit([1, 2, 3, 4, 5, 6, 7, 8], loadImg, 3);
+// loadByLimit([1, 2, 3, 4, 5, 6, 7, 8], loadImg, 3);
+
+const limitLoad = (array,fetch, limit = 3) => {
+    const sequence = [...array];
+
+    let promises = sequence
+        .splice(0, limit)
+        .map((url, index) => fetch(url).then(() => index));
+
+    let done = Promise.race(promises);
+
+    for (let i = 0; i < sequence.length; i++) {
+        done = done.then(resolvedIndex => {
+            promises[resolvedIndex] = fetch(sequence[i]).then(() => resolvedIndex);
+            return Promise.race(promises);
+        });
+    }
+};
+
+limitLoad([1, 2, 3, 4, 5, 6, 7, 8], loadImg, 3);
